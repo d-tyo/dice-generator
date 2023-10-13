07c7204ef5b5e1bd9b218a249f3dd9b7f212d590
d-tyo <dvichristyo@hotmail.com>
2023-10-13 14:47:42 +1100

    asdfgh

--- dicefunction.js
+++ dicefunction.js
@@ -17,13 +17,26 @@ let D6;
 let D10;
 
 
-function diceroll(number) {
+// function diceroll(number) {
 
-    if (number === 6)  {D6 = Math.floor(Math.random() * 6) + 1;  document.getElementById("D6").innerText = D6;}
-      else {D10 = Math.floor(Math.random() * 10) + 1; document.getElementById("D10").innerText = D10; }
-       console.log(D6)
+//     if (number === 6)  {D6 = Math.floor(Math.random() * 6) + 1;  document.getElementById("D6").innerText = D6;}
+//       else {D10 = Math.floor(Math.random() * 10) + 1; document.getElementById("D10").innerText = D10; }
+//        console.log(D6)
 
     //    document.getElementById("D6").innerText = D6;
     //    document.getElementById("D10").innerText = D10; {}
 
-} 
+// } 
+
+function diceroll(number) {
+
+  D6 = Math.floor(Math.random() * number)
+  document.getElementById("D6").innerText = D6;
+  
+  document.getElementById('D'+number).innerText = D6
+  
+  }
+  
+  
+  
+  // seperate function to  generate the random numbers line 33
